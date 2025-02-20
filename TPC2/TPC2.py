412d450bbb177879980b61304607a11c22c5edb6
Ana Carolina Penha Cerqueira <114937042+Cerqueira025@users.noreply.github.com>
2025-02-20 14:24:50 +0000

    Adicionada função obrasPorPeriodoNome

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -2,7 +2,7 @@ import re
 
 def getTexto():
   with open(r'C:\Users\Cerqueira\OneDrive\Área de Trabalho\PL\PL2025-A104188\TPC2\obras.csv', mode='r', encoding='utf-8') as file:
-    texto = file.read()  # Agora é uma única string
+    texto = file.read().strip()  # Agora é uma única string
 
   padrao = r'([^;]+);"(.*?)";([^;]+);([^;]+);([^;]+);([^;]+);([^;\n]+)'
   matches = re.findall(padrao, texto, re.DOTALL)
@@ -26,12 +26,25 @@ def obrasPorPeriodo(lista):
       periodos[obra[3]] += 1
   return periodos
 
+def obrasPorPeriodoNome(lista):
+  periodos = {}
+  for obra in lista:
+    if obra[3] not in periodos:
+      periodos[obra[3]] = [obra[0].lstrip()]
+    else:
+      periodos[obra[3]].append(obra[0].lstrip())
+  return periodos
+
 
 def main():
   entradas = getTexto()
 
+  #ex1
   print(compositores(entradas))
+  #ex2
   print(obrasPorPeriodo(entradas))
+  #ex3
+  print(obrasPorPeriodoNome(entradas))
 
 
 main()
