2d5151598a3b4f5dd2f63cdd1ad0bac5ea7a9ce7
Ana Carolina Penha Cerqueira <114937042+Cerqueira025@users.noreply.github.com>
2025-02-20 13:19:03 +0000

    Func 'getTexto()' a funcionar

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -1,50 +1,26 @@
 import re
 
-def compositores(list):
-  compositores = []
-  for obra in list:
-    compositores.append(obra[0])
-  return compositores.sort()
-
-
 def getTexto():
   with open(r'C:\Users\Cerqueira\OneDrive\Área de Trabalho\PL\PL2025-A104188\TPC2\obras.csv', mode='r', encoding='utf-8') as file:
-    texto = file.read().strip()
+    texto = file.read()  # Agora é uma única string
 
-  whitespace = ' \t\n\r\x0b\x0c'
-  entradas = []
-  bloco = []
-  i = 0
-  t = ""
-  for letra in texto:
-    if letra != ';':
-      if letra not in whitespace:
-        t += letra
-    else:
-      bloco.append(t)
-      t = ""
-      i+=1
-    if i == 6 and letra in whitespace:
-      bloco.append(t)
-      entradas.append(bloco)
-      bloco = []
-      t = ""
-      i=0
+  padrao = r'([^;]+);"(.*?)";([^;]+);([^;]+);([^;]+);([^;]+);([^;\n]+)'
+  matches = re.findall(padrao, texto, re.DOTALL)
 
-  #print(entradas)
-  return entradas
+  if matches:
+    print(matches)
+  return matches
 
-def ler_dataset():
-  with open(r'C:\Users\Cerqueira\OneDrive\Área de Trabalho\PL\PL2025-A104188\TPC2\obras.csv', "r", encoding="utf-8") as file:
-    texto = file.read()
+def compositores(lista):
+  compositores = []
+  for obra in lista:
+    compositores.append(obra[4])
+  return sorted(set(compositores))
+
 
 def main():
   entradas = getTexto()
-  #entradas = ler_dataset()
-  print(entradas)
 
-  #print(compositores(entradas))
+  print(compositores(entradas))
 
 main()
-
-#nome;desc;anoCriacao;periodo;compositor;duracao;_id
