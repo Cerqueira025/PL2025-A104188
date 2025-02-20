8a273181575b991c66606705dce81b5f5b04ce83
Ana Carolina Penha Cerqueira <114937042+Cerqueira025@users.noreply.github.com>
2025-02-20 13:29:10 +0000

    Funcao 'obrasPorPeriodo' adicionada

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -17,10 +17,21 @@ def compositores(lista):
     compositores.append(obra[4])
   return sorted(set(compositores))
 
+def obrasPorPeriodo(lista):
+  periodos = {}
+  for obra in lista:
+    if obra[3] not in periodos:
+      periodos[obra[3]] = 1
+    else:
+      periodos[obra[3]] += 1
+  return periodos
+
 
 def main():
   entradas = getTexto()
 
   print(compositores(entradas))
+  print(obrasPorPeriodo(entradas))
+
 
 main()
